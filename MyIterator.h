9fe14a876d4ccdebb0288f31454fac8ec325437b
Виктор Новсад <novosad_msu@mail.ru>
2016-11-13 17:02:49 +0300

    Add files via upload

--- MyIterator.h
+++ MyIterator.h
@@ -2,14 +2,14 @@
 #define _MYITERATOR_H_
 #include <string>
 
-template<class std::string, class T>
+template<class U = std::string, class T>
 class AVL_iterator {
 public:
 	AVL_iterator() : _node(NULL) {}
 	AVL_iterator(const AVL_iterator &other) : _node(other._node) {}
-	AVL_iterator(AVLNode<std::string, T> *other) : _node(other) {}
-	AVLNode<std::string, T> operator*() { return *_node; }
-	AVL_iterator & operator++() { ++_node; return *this; }
+	AVL_iterator(AVLNode<U, T> *other) : _node(other) {}
+	AVLNode<U, T> operator*() { return *_node; }
+	AVL_iterator< & operator++() { ++_node; return *this; }
 	bool operator==(const AVL_iterator &other);
 	bool operator<(const AVL_iterator &other);
 	bool operator!=(const AVL_iterator &other);
@@ -19,20 +19,20 @@ private:
 };
 
 
-template<class std::string, class T>
-bool AVL_iterator<std::string, T>::operator==(const AVL_iterator &other) {
+template<class U = std::string, class T>
+bool AVL_iterator<U, T>::operator==(const AVL_iterator &other) {
 	return (_node == other._node);
 }
-template<class std::string, class T>
-bool AVL_iterator<std::string, T>::operator!=(const AVL_iterator &other) {
+template<class U = std::string, class T>
+bool AVL_iterator<U, T>::operator!=(const AVL_iterator &other) {
 	return (_node != other._node);
 }
-template<class std::string, class T>
-bool AVL_iterator<std::string, T>::operator<(const AVL_iterator &other) {
+template<class U = std::string, class T>
+bool AVL_iterator<U, T>::operator<(const AVL_iterator &other) {
 	return (_node < other._node);
 }
-template<class std::string, class T>
-AVL_iterator& AVL_iterator<std::string, T>::operator=(const AVL_iterator> &other) {
+template<class U = std::string, class T>
+AVL_iterator& AVL_iterator<U, T>::operator=(const AVL_iterator> &other) {
 	_node = other._node;
 	return *this;
 }
